666e786cc21022f0fb328dbaead763882d7f9f9a
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-07-02 17:07:40 -0300

    adicionar validação regex aos campos de conteúdo e autoria

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 import api from "./api.js";
 import ui from "./interface.js";
 
+const regexContent = /^[A-Za-z\s]{10,}$/;
+const regexAuthor = /^[A-Za-z]{3, 15}$/;
+function validarContent(regex, conteudo) {
+  return regex.test(conteudo);
+}
+
 const formPensamento = document.getElementById('pensamento-form');
 const inputBusca = document.getElementById('campo-busca');
 
@@ -15,8 +21,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const autoria = document.getElementById('pensamento-autoria').value;
     const data = document.getElementById('pensamento-data').value;
 
+    if (!validarContent(regexContent, conteudo)) {
+      alert('É permitida a inclusão apenas de letras e espaços com no mínimo 10 caracteres.');
+      return
+    }
+    if (!validarContent(regexAuthor, autoria)) {
+      alert('É permitida a inclusão de letras e entre 3 e 15 caracteres sem espaços.');
+      return
+    }
     if (!validarData(data)) {
-      alert("Não é permitido o cadastro de datas futuras. Selecione outra data")
+      alert("Não é permitido o cadastro de datas futuras. Selecione outra data");
       return
     }
     try {
